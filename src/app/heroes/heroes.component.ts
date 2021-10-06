305bb6122520c1d438a1c2b972aa9fcf0817765b
Emily <emily.huang@Blackbaud.com>
2021-10-05 23:57:10 -0400

    making service available to the DI system and injecting it in the component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -13,15 +13,22 @@ export class HeroesComponent implements OnInit {
     id: 1,
     name: 'Windstorm'
   };
-  heroes = HEROES;
-  constructor() { }
+
+  heroes: Hero[] = [];
+
+  constructor(private heroService: HeroService) {}
 
   selectedHero?: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
+
   ngOnInit(): void {
+    this.getHeroes();
   }
 
 }
